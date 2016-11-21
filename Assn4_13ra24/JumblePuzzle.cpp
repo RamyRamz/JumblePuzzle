53bfa0631981e0e814bcfd2ae37f5f5eb53f38e1
Ramy Ayash <ramyayashbcs@gmail.com>
2016-11-20 22:27:30 -0500

    Updated files and added unit tests

--- Assn4_13ra24/JumblePuzzle.cpp
+++ Assn4_13ra24/JumblePuzzle.cpp
@@ -15,42 +15,160 @@ using namespace std;
 
 JumblePuzzle::JumblePuzzle(const string& toHide, const string& diff) {
 	len = toHide.length();
+	if(len < 3 || len > 10) //Checks to see that the word is not too short or too long
+		throw BadJumbleException("Length of word cannot be less than 3 or more than 18");
 
-	if(len < 3 || len > 18)
-		throw BadJumbleException("Length of word cannot be less than 3 or more than 18")
-
-	if (diff == "easy")
-		len *= 2;
-	else if (diff == "medium")
-		len *=3;
-	else if(diff == "hard")
-		len *= 4;
+	if (diff == "easy") {len *= 2;}
+	else if (diff == "medium") {len *=3;}
+	else if(diff == "hard") {len *= 4;}
 	else
-		throw BadJumbleException("Difficulty must be Easy, Medium, or Hard")
+		throw BadJumbleException("Difficulty must be easy, medium, or hard");
 
-	jumble = new char*[len]
+	matrix = new char*[len]; //Making a new Matrix
 
 	for (int i = 0; i < len; i++)
-		jumble[i] = new char[len]
-}
+		matrix[i] = new char[len]; //Making the Matrix 2D, Adding columns
 
-	const char* letters = "abcdefghijklmnopqrstuvwxyz";
+	const char* letters = "abcdefghijklmnopqrstuvwxyz"; //Available letters
 
 	srand(time(NULL));
 
-	for (int i = 0; i < len; i++)
-		for (int j = 0; i < len; j++)
-			jumble[i][j] = (char) letters[rand() % 25]
+	for(int i = 0; i< len; i++)
+		for(int j = 0; j < len; j++)
+			matrix[i][j] = (char) letters[rand() % 25];
 
 	row = rand() % len;
 	col = rand() % len;
 
-	jumble[row][col] = toHide.at(0)
+	matrix[row][col] = toHide.at(0);
+	const char* dir = "nesw";
+	bool placed = false;
 
-JumblePuzzle::~JumblePuzzle() {
-	// TODO Auto-generated destructor stub
+	while(!placed){
+		// Pick a random direction
+				direction = dir [rand() % 4];
+
+				if (direction == 'n'){ //Moving up
+					for (int i = 1; i < toHide.length(); i++)
+					{
+						// Out of bounds
+						if ((row-i) < 0)
+							break;
+						else if (i == (toHide.length() - 1)){ // last letter
+
+							matrix[row-i][col] = toHide.at(i);
+							placed = 1;
+						}
+						else
+							matrix[row-i][col] = toHide.at(i);
+					}
+				}
+				else if (direction == 'e'){ // Moving right
+					for (int i = 1; i < toHide.length(); i++){
+						if ((col+i) > (toHide.length()-1))
+							break;
+						else if (i == (toHide.length() - 1)){
+							matrix[row][col+i] = toHide.at(i);
+							placed = 1;
+						}
+						else
+							matrix[row][col+i] = toHide.at(i);
+					}
+				}
+				else if (direction == 's'){
+					// Going down
+					for (int i = 1; i < toHide.length(); i++){
+						if ((row+i) > (toHide.length()-1))
+							break;
+						else if (i == (toHide.length() - 1)){
+							matrix[row+i][col] = toHide.at(i);
+							placed = 1;
+						}
+						else
+							matrix[row+i][col] = toHide.at(i);
+					}
+				}
+				else{ //Going left
+					for (int i = 1; i < toHide.length(); i++){
+						if ( (col-i) < 0 )
+							break;
+						else if (i == (toHide.length() - 1)){
+							matrix[row][col-i] = toHide.at(i);
+							placed = 1;
+						}
+						else
+							matrix[row][col-i] = toHide.at(i);
+					}
+				}
+			}
+	}
+
+JumblePuzzle::JumblePuzzle(const JumblePuzzle& right){
+	len = right.getSize();
+	row = right.getRowPos();
+	col = right.getColPos();
+	direction = right.getDirection();
+	matrix = new char*[len];
+
+	for (int i = 0; i < len; i++)
+		matrix[i] = new char[len];
+
+	charArrayPtr* rightMatrix = right.getJumble();
+
+	for (int i = 0; i < len; i++){
+		for (int j = 0; j < len; j++)
+			matrix[i][j] = rightMatrix[i][j];
+	}
 }
 
+int JumblePuzzle::getSize() const { return len; }
+int JumblePuzzle::getRowPos() const { return row; }
+int JumblePuzzle::getColPos() const { return col; }
+char JumblePuzzle::getDirection() const { return direction; }
+
+JumblePuzzle::~JumblePuzzle() {
+	for(int i = 0; i < len; i++)
+		delete matrix[i];
+	delete[] matrix;
+}
+
+JumblePuzzle& JumblePuzzle::operator=(const JumblePuzzle& right){
+	if(this != &right)
+	{
+		row = right.getRowPos();
+		col = right.getColPos();
+		direction = right.getDirection();
+		len = right.getSize();
+
+		for(int i = 0; i < len; i++)
+			delete[] matrix[i];
+		delete[] matrix;
+		matrix = new char*[len];
+		for(int i = 0; i < len; i++)
+			matrix[i] = new char[len];
+
+		charArrayPtr* rightJumble = right.getJumble();
+
+		for (int i = 0; i < len; i++){
+			for(int j = 0; j < len; j++)
+				matrix[i][j] = rightJumble[i][j];
+		}
+	}
+	return *this;
+}
+
+charArrayPtr* JumblePuzzle::getJumble() const{
+	charArrayPtr* newMatrix = new char*[len];
+
+	for (int i = 0; i < len; i++)
+		newMatrix[i] = new char[len];
+
+	for (int i = 0; i < len; i++){
+		for (int j = 0; j < len; j++)
+			newMatrix[i][j] = matrix[i][j];
+	}
+	return newMatrix;
+}
 
 BadJumbleException::BadJumbleException(const string& e) : message(e){ }
-string& BadJumbleException::what() const { return message; }
+string& BadJumbleException::what() { return message; }
--- Assn4_13ra24/JumblePuzzle.h
+++ Assn4_13ra24/JumblePuzzle.h
@@ -1,26 +1,31 @@
 /*
  * JumblePuzzle.h
  *
- *  Created on: Nov 8, 2016
- *      Author: ramy
+ *  Created on: Nov 10, 2016
+ *      Author: Ramy Ayash - 13ra24 - 10130200
+ *      This is Assignment 4, Course CISC 320
+ *      The JumblePuzzle is a 2D matrix containing a word inputted by the user that is to be hidden
+ *      The JumblePuzzle is stored on the heap, and is deleted after the user has figured out the location of the word
+ *      The BadJumbleException class is included and provides customized error messages to the users
  */
 #include<iostream>
 #include<exception>
 #include<string>
+#include<stdlib.h>
 using namespace std;
 typedef char* charArrayPtr;
 
 class JumblePuzzle {
 	public:
-		JumblePuzzle(const string&, const string&);
-		JumblePuzzle(const JumblePuzzle&);
-		~JumblePuzzle();
-		JumblePuzzle& operator=(const JumblePuzzle&);
-		charArrayPtr* getJumble();
-		int getSize();
-		int getRowPos();
-		int getColPos();
-		char getDirection();
+		JumblePuzzle(const string& toHide, const string& diff); //Constructor that takes a word and a difficulty, Takes 2 strings: Word to be hidden and Difficulty
+		JumblePuzzle(const JumblePuzzle& right); //Copy Constructor
+		~JumblePuzzle(); //Destructor, gets rid of the matrix object on the heap when the player has guessed the word or terminated the program
+		JumblePuzzle& operator=(const JumblePuzzle& right); //Assignment operator overloading
+		charArrayPtr* getJumble() const; //Getter for the matrix
+		int getSize() const; //Getter for the size of the matrix
+		int getRowPos() const; //Gets the row position, mostly to check if the user is correct
+		int getColPos() const; //Gets the column position, mostly to check if the user is correct
+		char getDirection() const; //Fetches the direction of the rest of the word from the user
 	private:
 		int row;
 		int col;
@@ -31,8 +36,8 @@ class JumblePuzzle {
 
 class BadJumbleException {
 public:
-	BadJumbleException(const string&); //Gives a customized message for File Reading/Writing errors
-	string& what() const;
+	BadJumbleException(const string&); //Gives a customized message for Jumble Exceptions
+	string& what();
 private:
 	string message;
 };
